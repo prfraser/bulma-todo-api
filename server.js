cd54bb47a0475c8a06c8e24157c8e49adb3759e8
philip fraser <pr.fraser3@gmail.com>
2017-11-30 15:05:35 +1000

    added mongoose

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
-const tasks = [
-      { id: 1, todo: 'Buy BTC', time: '29/11/2017, 13:26:50', complete: true },
-      { id: 2, todo: 'Do some coding', time: '29/11/2017, 13:27:50', complete: false }
-		]
+const Task = require('./models/task');
 
 app.use(bodyParser.json());
 
 app.get('/api/tasks', (req, res) => {
 	// Send the tasks array to the user
-	res.send(tasks)
+	Task.find().then((result) => {
+		console.log(result)
+		res.send(result)
+	})
 });
 
 app.post('/api/tasks', (req, res) => {
 	console.log(req.body)
-	let task = req.body
-	tasks.push(task)
-	res.send(task)
+	let task = new Task ({ todo: req.body.todo, time: new Date(), complete: req.body.complete })
+	task.save((err, task) => {
+	  if (err) {
+	    console.log(err);
+	  } else {
+	    console.log('Task created');
+	  }
+	  res.send(task);
+	})
 })
 
 app.listen(8000, () => console.log('Bulma todo api serevr listening on port 8000!'));
